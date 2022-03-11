2285bc8d2f091b346e4042d9bdc66e24cbd23c7b
kandcom <korkinav8@gmail.com>
2022-03-11 15:14:50 +0600

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets = [];
+  let array = [];
+
+  for (let i = 0, length = str.length; i < length; i++) {
+    array.push(str[i]);
+  }
+  if (array.length % 2 !== 0) {
+      return false;
+  }
+  for (let i = 0, length = bracketsConfig.length; i < length; i++) {
+      openBrackets.push(bracketsConfig[i][0]);
+      closeBrackets.push(bracketsConfig[i][1]);
+  }
+  for (let i = array.length - 1; i >= 0; i--) {
+          for (let j = 0, length = openBrackets.length; j < length; j++) {
+              if (array[i] === openBrackets[j] && array[i + 1] === closeBrackets[j]) {
+                array.splice(i, 2);
+              }
+          }
+  }
+  return (array.length === 0);
 }
